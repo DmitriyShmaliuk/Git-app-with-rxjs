ef6201172933701795defefbdd08fd16229e5bbd
Dmytriy Shmaliuk <dimkas2001@icloud.com>
2020-10-29 22:04:15 +0200

    add (index.js): hbs as view engine

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const path = require("path");
 const { DEFAULT_PORT } = require("./constants");
 const homeRoutes = require("./routes/homeRoutes");
 
 const PORT = process.env.PORT || DEFAULT_PORT;
-
 const app = express();
+
+app.engine("hbs", exphbs());
+app.set("view engine", "hbs");
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use("/", homeRoutes);
 
 app.listen(PORT, () => {
