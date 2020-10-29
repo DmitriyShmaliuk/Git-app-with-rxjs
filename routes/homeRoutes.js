5e4b9cf3c70da51b178deebe8f72347ebdc64afd
Dmytriy Shmaliuk <dimkas2001@icloud.com>
2020-10-29 22:02:29 +0200

    add (routes/homeRoutes.js): home routes handlers

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,38 @@
 const { Router } = require("express");
+const axios = require("axios");
+const { SEARCH_REQUEST } = require("../constants");
+
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.sendFile('/index.html');
+const pageInfo = {
+  userName: "",
+  users: [],
+};
+
+router.get("/", (req, res) => {
+  res.render("index", {
+    ...pageInfo,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const { data } = await axios.get(SEARCH_REQUEST + name.trim());
+    const { items: users } = data;
+
+    pageInfo.userName = name;
+    pageInfo.users = users.map(({ login, avatar_url, html_url}) => ({
+      name: login,
+      avatar: avatar_url,
+      profileLink: html_url
+    }));
+
+    res.redirect("/");
+  } catch (err) {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
